2779bb0dd2d5c7bed07cdf66352b6e61d149b383
Patrícia Vasques <patriciahhhh@gmail.com>
2023-04-12 08:46:51 -0300

    feat: criação página login

---
+++ src/components/Login.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+export function Login () {
+    return (
+        <form className="form-container">
+
+            <div className="form-group">
+                <label className="label-name" htmlFor="name">Usuário</label>
+                <div className="input-name">
+                    <input type="text" className="" name="usuario" id="usuario" placeholder="Digite seu usuário" required/>
+                </div>
+            </div>
+
+            <div className="form-group">
+                <label className="label-password" htmlFor="name">Senha</label>
+                <div className="">
+                    <input type="password" className="input-password" name="senha" id="senha" placeholder="Digite sua senha" required/>
+                </div>
+            </div>
+        </form>
+    )
+}
