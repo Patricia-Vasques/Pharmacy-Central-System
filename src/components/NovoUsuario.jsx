5d36a4c5c7b1c212b3a4164e66fb82ed69a5c6ab
Patrícia Vasques <patriciahhhh@gmail.com>
2023-04-15 13:54:03 -0300

    feat: acrescentando o input usuário, melhorando o botão cadastrar

--- src/components/NovoUsuario.jsx
+++ src/components/NovoUsuario.jsx
@@ -2,22 +2,34 @@ import React from "react";
 import { Header } from "./Header";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+
 import "./novoUsuario.css"
 
 export function NovoUsuario ({cadastrarUsuario}) {
     const [nome, setNome] = useState('');
     const [email, setEmail] = useState('');
+    const [usuario, setUsuario] = useState('');
     const [senha, setSenha] = useState('');
+    
+ 
 
-    function handleCadastrarUsuario() {
-       
-        
-        if (nome && email && senha !== ''){
+    function handleCadastrarUsuario() {{
+
+        if (nome !== '' && usuario !== '' && senha !== '' & senha >= 8 && senha /[0-9] [A-Za-z] !== ""){
+            console.log("nome:", nome);
+            console.log("email:", email);
+            console.log("usuario:", usuario);
+            console.log("senha.:", senha);
+        } else{
+            alert("Preencha todos os campos!")
+        }
             cadastrarUsuario({ nome, email, senha });
             setNome([]);
-            setSenha([]);
+            setEmail([]);
+            setUsuario([]);
             setSenha([]);
         }
+
     }
 
 
@@ -25,7 +37,11 @@ export function NovoUsuario ({cadastrarUsuario}) {
         < div>
         <Header />
 
-        <form className="form-novoUsuario">
+        <div className="text-novo-usuario">
+            <h1>Cadastro Novo Usuário</h1>
+        </div>
+
+        <form className="form-novoUsuario" >
 
         <div className="form-group">
         <label className="label-novoUsuario-name" htmlFor="name"><b>Nome Completo:</b></label>
@@ -42,9 +58,16 @@ export function NovoUsuario ({cadastrarUsuario}) {
         </div>
 
         <div className="form-group">
+        <label className="label-novoUsuario-usuario" htmlFor="usuarioi"><b>Usuário:</b></label>
+        <div className="form-novoUsuario-usuario">
+            <input type="text" className="input-novoUsuario-usuario" name="novoUsuario" id="novoUsuario" placeholder="Digite um usuário" onChange={(e) => setEmail(e.target.value)} required/>
+        </div>
+        </div>
+
+        <div className="form-group">
         <label className="label-novoUsuario-senha" htmlFor="senha"><b>Senha:</b></label>
         <div className="form-novoUsuario-senha">
-            <input type="password" className="input-novoUsuario-senha" name="senha" id="senha" placeholder="Digite uma senha" onChange={(e) => setSenha(e.target.value)} required/>
+            <input type="password" className="input-novoUsuario-senha" name="senha" id="senha"  placeholder="Digite uma senha" onChange={(e) => setSenha(e.target.value)} required="Senha deve conter 8 caracteres entre números e letras!"/>
         </div>
         </div>
 
@@ -57,3 +80,4 @@ export function NovoUsuario ({cadastrarUsuario}) {
     )
 }
 
+//ref='({minLength: 8, pattern:"/^[0-9] [A-Za-z]" }}'
