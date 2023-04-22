030f8083ef64e7f75e510b0e5f7ef78dc49c6012
Patrícia Vasques <patriciahhhh@gmail.com>
2023-04-21 21:05:49 -0300

    tirando alguns espaços vazios no código

--- src/components/NovoUsuario.jsx
+++ src/components/NovoUsuario.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { Header } from "./Header";
-import { Link } from "react-router-dom";
 import "./novoUsuario.css"
 
 export function NovoUsuario () {
