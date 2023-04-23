7db2e9548a87ed344e2fdfb1e4d84e369d6d9058
Patrícia Vasques <patriciahhhh@gmail.com>
2023-04-23 16:24:46 -0300

    feat: fiz a alteração do cógido usando props e propTypes para melhorias

---
+++ src/components/CadastroMedicamentos.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import PropTypes from "prop-types";
+import{ useState } from "react"
+import { Header } from "./Header";
+import "./cadastroMedicamentos.css"
+
+export const CadastroMedicamentos = (props) => {
+    const [nomeMedicamento, setNomeMedicamento] =useState('');
+    const [laboratorioMedicamento, setLaboratorioMedicamento] = useState('');
+    const [dosagemMedicamento, setDosagemMedicamento] = useState('');
+    const [descricaoMedicamento, setDescricaoMedicamento] = useState('');
+    const [precoMedicamento, setPrecoMedicamento] = useState('');
+    const [tipoMedicamento, setTipoMedicamento] = useState('');
+
+    function handleCadastrarMedicamentos (event) {
+        event.preventDefault();
+
+        const medicamento ={
+            nomeMedicamento: nomeMedicamento,
+            laboratorioMedicamento: laboratorioMedicamento,
+            dosagemMedicamento: dosagemMedicamento,
+            descricaoMedicamento: descricaoMedicamento,
+            precoMedicamento: precoMedicamento,
+            tipoMedicamento: tipoMedicamento,
+        };
+
+        alert("Medicamento cadastrado com sucesso!");
+
+        var listaMedicamentos = JSON.parse(localStorage.getItem("listaMedicamentos")) || [];
+
+        localStorage.setItem("listaMedicamentos", JSON.stringify(listaMedicamentos))
+    
+        props.quandoCadastrar(medicamento);
+
+        //resetar os valores 
+        setNomeMedicamento("");
+        setLaboratorioMedicamento("");
+        setDosagemMedicamento("");
+        setDescricaoMedicamento("");
+        setPrecoMedicamento("");
+        setTipoMedicamento("");
+    }
+
+    return (
+        <div>
+            < Header/>
+            <form className="form-cadastro-medicamentos" onSubmit={handleCadastrarMedicamentos}>
+
+                <h2 className="text-cadastro-medicamentos">Cadastro de Medicamentos</h2>
+                
+                <div className="form-group">
+                    <label className="label-cadastroMedicamento" htmlFor="nome-medicamento">Nome do Medicamento:</label>
+                    <input className="input-cadastroMedicamento" type="text" name="nome -medicamento" id="nome-medicamento" placeholder="Nome do medicamento"  onChange={(e) => setNomeMedicamento(e.target.value)} />
+                </div>
+
+                <div className="form-group">
+                    <label className="label-cadastroMedicamento" htmlFor="nome-laboratorio">Nome do Laboratório:</label>
+                    <input className="input-cadastroMedicamento" type="text" name="nome-laboratorio" id="nome-laboratorio" placeholder="Nome do laboratório"   onChange={(e) => setNomeLaboratorio(e.target.value)}/>
+                </div>
+
+                <div className="form-group">
+                    <label className="label-cadastroMedicamento" htmlFor="dosagem-medicamento">Dosagem do medicamento:</label>
+                    <input className="input-cadastroMedicamento" type="text" name="dosagem-medicamento" id="dosagem-medicamento" placeholder="Dosagem do medicamento"  onChange={(e) => setDosagemMedicamento(e.target.value)} />
+                </div>
+
+                <div className="form-group">
+                    <label className="label-cadastroMedicamento" htmlFor="descricao-medicamento">Descrição do medicamento:</label>
+                    <textarea className="input-cadastroMedicamento"  name="descricao-medicamento" id="descricao-medicamento" placeholder="Descrição do medicamento"  onChange={(e) => setDescricaoMedicamento(e.target.value)}/>
+                </div>
+
+                <div className="form-group">
+                    <label className="label-cadastroMedicamento" htmlFor="preco-medicamento">Preço: R$</label>
+                    <input className="input-cadastroMedicamento" type="text" min="0" name="preco-medicamento" id="preco-medicamento" placeholder="Preço" onChange={(e) => setPrecoMedicamento(e.target.value)} />
+                </div>
+
+                <div className="form-group">
+                    <label className="label-cadastroMedicamento" htmlFor="tipo-medicamento">Tipo do medicamento:</label>
+                    <select className="input-cadastroMedicamento"  name="tipo-medicamento" id="tipo-medicamento"   onChange={(e) => setTipoMedicamento(e.target.value)} required>
+                        <option value="medicamento-controlado">Controlado</option>
+                        <option value="medicamento-comum">Comum</option>
+                    </select>
+
+                    <button className="button-cadastro-medicamento"> Cadastrar </button>
+                </div>
+
+            </form>
+        </div>
+    )}
+    
+
+    CadastroMedicamentos.propTypes = {
+        quandoCadastrar: PropTypes.func.isRequired
+    }
