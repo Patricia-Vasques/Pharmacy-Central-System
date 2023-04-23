d701efdb2b2f5a0d7c5f3ecf7761817280c7b6fd
Patrícia Vasques <patriciahhhh@gmail.com>
2023-04-23 16:23:27 -0300

    feat: criação da função  para  receber a farmácia e o medicamento como argumentos

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import { Login } from "./components/Login"
 import { NovoUsuario } from "./components/NovoUsuario"
 import { ListaDeFarmacias } from "./components/ListaDeFarmacias"
 import { CadastrarFarmacia } from "./components/CadastrarFarmacia"
-import { CadastroMedicamentos } from "./CadastroMedicamentos"
-
+import { CadastroMedicamentos } from "./components/CadastroMedicamentos"
+import { ListaDeMedicamentos } from "./components/ListaDeMedicamentos"
 import './App.css'
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
   //Estado
   // useState que inicia com array vazio para futuramente receber o array de farmácias
   const [farmacias, setFarmacias] = useState([]);
+
 
   //useEffect com array vazio, pois só vai executar a primeira aplicação carregar
   //Busca as farmácias no localStorage e se esxistir da um setFarmacias para guardar as farmacias do storage no useState
@@ -25,22 +26,38 @@ function App() {
     }
   }, []);
 
-  //função para receber uma farmácia como argumento e adiciona ao estado jutno com as farmácias que estão lá
-  const cadastrarFarmacia = (farmacia) => {
-    setFarmacias([...farmacias,farmacia])
+  const [medicamentos, setMedicamentos] =useState([]);
+
+  useEffect(() => {
+    const medicamentosDoStorage = localStorage.getItem('medicamentos')
+  if(medicamentosDoStorage){
+    setMedicamentos(JSON.parse(medicamentosDoStorage))
   }
+}, []);
+
+  //função para receber uma farmácia como argumento e adiciona ao estado junto com as farmácias que estão lá
+  const cadastrarFarmacia = (farmacia) => {
+    setFarmacias([...farmacias, farmacia])
+  }
+
+  const cadastrarMedicamento = (medicamento) =>{
+    setMedicamentos([...medicamentos, medicamento])
+  }
+
+  
+
   return (
     <div className="App">
-
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/NovoUsuario" element={<NovoUsuario />} />
+          <Route path="/NovoUsuario" element={<NovoUsuario />} />.ll
           <Route path="/ListaDeFarmacias" element={<ListaDeFarmacias />} />
           <Route path="/CadastrarFarmacia" element={<CadastrarFarmacia />} />
-          <Route path="/CadastroMedicamentos" element={< CadastroMedicamentos />} />
+          <Route path="/CadastroMedicamentos" element={< CadastroMedicamentos quandoCadastrar={cadastrarMedicamento}/>} />
+          <Route path="/ListaDeMedicamentos" element={< ListaDeMedicamentos />} />
           <Route path="/ListaDeFarmacias" element={<ListaDeFarmacias lista={farmacias} />} />
-          <Route path="/CadastrarFarmacia" element={<CadastrarFarmacia cadastrarFarmacia={cadastrarFarmacia} />} />
+          <Route path="/CadastrarFarmacia" element={<CadastrarFarmacia quandoCadastrarFarmaci={cadastrarFarmacia} />} />
         </Routes>
       </Router>
 
