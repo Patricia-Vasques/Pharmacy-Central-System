0ddc05b5cee07d9c91aadb2278d32f99d353f3a1
Patrícia Vasques <patriciahhhh@gmail.com>
2023-04-26 11:56:49 -0300

    feat: usei bootstrap para fazer o card

--- src/components/ListaDeMedicamentos.jsx
+++ src/components/ListaDeMedicamentos.jsx
@@ -1,58 +1,77 @@
 import { Header } from "./Header";
-import { useState } from "react";
-import Modal from "react-modal";
-import Card from "react-bootstrap/Card";
+import { useState, useEffect } from "react";
+import { Modal, Card, ListGroup, ListGroupItem, Button } from "react-bootstrap"
 import "./listaMedicamentos.css";
 
 Modal.setAppElement("#root")
 
-export function ListaDeMedicamentos({medicamentos, laboratorio, tipo, dosagem, preco, descricao}){
-    
-    const  listaMedicamentos = JSON.parse(localStorage.getItem("listaMedicamentos")) || [];
+export function CadastroMedicamentos () {
+    const [listaMedicamentos, setListaMedicamentos] = useState([])
+    const [medicamentoSelecionado, setMedicamentoSelecionado] = useState(null)
 
-    const [modalIsOpen, setModalIsOpen] = useState(false);
+    useEffect(() => {
+        const listaMedicamentosStorage = localStorage.getItem("listaMedicamentos");
+        if (listaMedicamentosStorage){
+            setListaMedicamentos(JSON.parse(listaMedicamentosStorage))
+        }
+    }, [])
 
     return(
-        <div>
-            <div className="title-card">
-                <Header/>
-            <p><b>Lista de Medicamentos Cadastrados</b></p>
-            </div>
-
-            <div className="card" data-bs-toggle="modal"
-            data-bs-target={Card}>
-                <img src="https://th.bing.com/th/id/OIP.AuxQBiLqdNx2b5pe9eeseAAAAA?pid=ImgDet&rs=1" alt="Foto do medicamento conforme a liberação da ANVISA" width="120px" />
-                <h5 className="title">{medicamentos}</h5>
-                <h6 className="subTitle md-1 text-muted">{dosagem}</h6>
-                <p className="text">{descricao ? descricao.substring(0,40) : null }...</p>
-
-                <div className="d-grid col-6">
-                    
-                </div>
-
-            <div className="card-modal">
-                <button type="button" className="btn btn-outline-info btn-sm mt4"
-                data-bs-toggle="modal"
-                data-ds-target="#exempleModa4"
-                onClick={() =>setModalIsOpen (true) }>Detalhes</button>
-            </div>
-            </div>
+         <div className="card data-bs-toggle">
+            <Header />
         
-        <Modal 
-        isOpen={modalIsOpen}
-        onRequestClose={() => setModalIsOpen(false)}
-        style={{content:{color: "black"}, }}>
-        <h2>Detalhes do medicamento</h2>
-        <p><strong>Medicamento:</strong>{medicamentos}</p>
-        <p><strong>Laboratório:</strong>{laboratorio}</p>
-        <p><strong>Tipo:</strong>{tipo}</p>
-        <p><strong>Dosagem:</strong>{dosagem}</p>
-        <p><strong>Preço:</strong>{preco}</p>
-        <p><strong>Descrição:</strong>{descricao}</p>
+        <Card key={medicamento} style={{width:"250px"}}>
+            <Card.Img    style={{
+                width: "200px",
+                margin: "auto",
+              }}
+              src="https://th.bing.com/th/id/OIP.AuxQBiLqdNx2b5pe9eeseAAAAA?pid=ImgDet&rs=1" alt="Foto do medicamento conforme a liberação da ANVISA" width="120px" />
+             {listaMedicamentos.map((medicamento) => {
+                return(
+                    <div>
+              <Card.Title>{medicamento?.nomeMediamento}</Card.Title>
+              <Card.Text>{medicamento?.labotarorioMedicamento}</Card.Text>
+              </div>
 
-        <button type="button" clasName="btn btn-secundary me-2" data-bs-dismiss="modal"
-        onClick={() => setModalIsOpen(false)}>Fechar</button>
+               )
+              })}
+              <Button className="btn btn-outline-secondary" onClick={() => setMedicamentoSelecionado(medicamento)}>Detalhes</Button>
+             
+                <Modal show={medicamentoSelecionado} onHide={() => setMedicamentoSelecionado(null)}>
+            <Modal.Header>Titulo</Modal.Header>
+            <Modal.Body>
+                <ListGroup>
+
+                    <ListGroupItem>
+                        Medicamento: {medicamentoSelecionado?.nomeMedicamento}
+                    </ListGroupItem>
+
+                    <ListGroupItem>
+                        Laboratório: {medicamentoSelecionado?.laboratorioMedicamento}
+                    </ListGroupItem>
+
+                    <ListGroupItem>
+                        Dosagem: {medicamentoSelecionado?.dosagemMedicamento}
+                    </ListGroupItem>
+
+                    <ListGroupItem>
+                        Descrição: {medicamentoSelecionado?.descricaoMedicamento}
+                    </ListGroupItem>
+
+                    <ListGroupItem>
+                        Preço: {medicamentoSelecionado?.precoMedicamento}
+                    </ListGroupItem>
+
+                    <ListGroupItem>
+                        Tipo: {medicamentoSelecionado?.tipoMedicamento}
+                    </ListGroupItem>
+
+                </ListGroup>
+            </Modal.Body>
         </Modal>
-
+              )
+              </Card>
         </div>
-)}
+    )
+}
+
